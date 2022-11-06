d0c28777022f4c24bb84c7e9e96425ef83fa8427
jack <jack@cool-pi.com>
2022-11-06 10:48:03 +0800

    add dvi 1440x900 support

--- common/edid.c
+++ common/edid.c
@@ -1613,6 +1613,12 @@ static const struct base_drm_display_mode resolution_white[] = {
 			8408, 9000, 4320, 4336, 4356, 4400, 0,
 			DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_PVSYNC),
 	.vrefresh = 60, .picture_aspect_ratio = HDMI_PICTURE_ASPECT_16_9, },
+	/* 33. vic:47 - 1440x900@60Hz 256:135 */
+    { DRM_BASE_MODE(106500, 1440, 1520,
+            1672, 1904, 900, 903, 909, 934, 0,
+            DRM_MODE_FLAG_NHSYNC | DRM_MODE_FLAG_PVSYNC),
+	.vrefresh = 60, .picture_aspect_ratio = HDMI_PICTURE_ASPECT_256_135, },
+
 };
 
 struct minimode {
